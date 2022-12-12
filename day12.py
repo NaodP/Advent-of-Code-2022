6afd6b672f89bdecf20319572338e52b2ddb0944
NaodP <nalf4459@gmail.com>
2022-12-12 14:37:21 -0600

    Update day12.py

--- day12.py
+++ day12.py
@@ -8,7 +8,6 @@ def plot(seen):
             else: print(' ', end='')
         print()
 
-
 def part1(world, start):
     seen = set()
     queue = [(start)]
@@ -59,12 +58,36 @@ def part2(world):
     
     return minimum
 
+def part2Better(world, start):
+    seen = set()
+    queue = [(start)]
+    xMax = len(world)
+    yMax = len(world[0])
+
+    while queue:
+        current = queue.pop(0)
+        if world[current[0]][current[1]] == 'a': return current[2]
+        seen.add((current[0],current[1]))
+        curr = ord(world[current[0]][current[1]])
+        directions = [(current[0]-1, current[1]), (current[0]+1, current[1]), (current[0], current[1]-1), (current[0], current[1]+1)]
+
+        # Top
+        for dir in directions:
+            if dir not in seen and (dir[0] > -1 and dir[0] < xMax) and (dir[1] > -1 and dir[1] < yMax):
+                new = ord(world[dir[0]][dir[1]]) 
+                if new - curr > -2:
+                    queue.append((dir[0],dir[1],current[2]+1))
+                    seen.add(dir)
+    
+    return part1(world,(20,52,0))
+
 def main():
     file = open('day12-input.txt')
     world = []
     for line in file: world.append(list(line.strip()))
     print('Part 1 -', part1(world, (20,0,0)))
-    print('Part 2 -', part2(world))
+    print('Part 2 -', part2(world))                    # Initial Solution
+    print('Part 2* -', part2Better(world, (20,52,0)))  # Better Solution
 
 # Start
 main()
