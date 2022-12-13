4272eec9b350aba2a6f9bbd252a9fb06c2967b86
NaodP <nalf4459@gmail.com>
2022-12-12 21:52:02 -0600

    Update day13.py

---
+++ day13.py
@@ -0,0 +1,20 @@
+# Naod Philemon
+# 12/13/2022
+
+
+
+def part1():
+    pass
+def part2():
+    pass
+
+def main():
+    file = open('day13-input.txt')
+    for line in file: 
+        pass
+
+    print('Part 1 -', part1())
+    print('Part 2 -', part2())
+
+# Start
+main()
