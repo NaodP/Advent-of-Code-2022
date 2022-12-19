6a046b74ce9f0de2e29f2d567e74937ff07b54fc
NaodP <nalf4459@gmail.com>
2022-12-18 19:36:48 -0600

    Day 19

---
+++ Day 19/day19.py
@@ -0,0 +1,20 @@
+# Naod Philemon
+# 12/19/2022
+
+def part1():
+    pass
+
+def part2():
+    pass
+
+def main():
+    file = open('Day 19/day19-input.txt')
+
+    for line in file:
+        pass
+
+    print('Part 1 -', part1())
+    print('Part 2 -', part2())
+
+# Start
+main()
