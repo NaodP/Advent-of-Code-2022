f90a3bbdd3712da6db3a52d289254e87a36b34a2
NaodP <nalf4459@gmail.com>
2022-12-20 12:01:46 -0600

    Day 19

--- Day 19/day19.py
+++ Day 19/day19.py
@@ -2,6 +2,15 @@
 # 12/19/2022
 
 def part1():
+    # Use Dynamic Programming
+    # One Table For Each Robot
+    # Cost/Buy
+# T # +---------+
+# I # |         |
+# M # |         |
+# E # |         |
+    # |         |
+    # +---------+
     pass
 
 def part2():
@@ -11,6 +20,7 @@ def main():
     file = open('Day 19/day19-input.txt')
 
     for line in file:
+        line.strip().split('.')
         pass
 
     print('Part 1 -', part1())
